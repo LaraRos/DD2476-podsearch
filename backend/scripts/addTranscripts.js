6a390017dca4bb267399c4d23f46e48108c57219
Lara Rostami <rostami.lara@gmail.com>
2021-04-16 11:26:20 +0200

    add backend scripts

---
+++ backend/scripts/addTranscripts.js
@@ -0,0 +1,31 @@
+const indexFunctions = require('../info.js')
+const fs = require('fs');
+// Path to your reduced spotify transcript folder
+const folder = ''
+// replace with num of files, we only write 100 files right now
+// const count = 10
+
+const nextFiles = (currentFolder) => {
+    return fs.readdirSync(currentFolder)
+}
+
+const files = nextFiles(folder)
+let transcriptCount = 0
+
+for (i = 0; i < files.length; i++) {
+    const file = files[i]
+    const data = JSON.parse(fs.readFileSync(folder + "/" + file));
+    const numTranscripts = data.transcripts.length
+    for (j = 0; j < numTranscripts; j++) {
+        let newData = {
+            podcast_name: data.podcast_name,
+            data: data.transcripts[j].transcript,
+            start: data.transcripts[j].start,
+            end: data.transcripts[j].end,
+            offset: j
+        }
+        indexFunctions.addDocById(transcriptCount, newData)
+        transcriptCount = transcriptCount + 1
+    }
+    console.info("Logged " + count + " files")
+}
---
+++ backend/scripts/test.js
@@ -0,0 +1,3 @@
+const indexFunctions = require('../info.js')
+indexFunctions.addDocById(1, body)
+indexFunctions.addDoc(body)
