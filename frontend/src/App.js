bdfd969a501619faffc55631a034072f18abb748
Moa Andersson <moaandersson@Moas-MacBook-Pro.local>
2021-04-20 10:21:42 +0200

    started a bit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,16 @@ import {fetchQuery} from './search'
 
 function App() {
   const [queryString, setQueryString] = useState("")
+  const colors = ['#4285F4','#DB4437','#F4B400','#4285F4','#0F9D58', '#DB4437']
   return (
     <div className="App">
-      <h1>
-        Podcast search
-      </h1>
+      <div className="header">
+        {'PodcastSearch'.split('').map((c,i) => 
+        <span style={{color: colors[i%colors.length]}}>
+        {c}
+        </span>
+        )}
+      </div>
         <input 
           type='text'
           value={queryString}
