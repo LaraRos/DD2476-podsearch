f8c281f6f9d2b221c78f23acdaa8ab14652f03be
Moa Andersson <mmoahandersson@gmail.com>
2021-04-23 10:44:31 +0200

    la in sökresultat

--- frontend/src/ResultPage.js
+++ frontend/src/ResultPage.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import {useState} from 'react';
 import Logo from './Logo.js'
-function ResultPage({queryString, setQueryString}) {
+import SearchField from './SearchField.js'
+import  { Redirect } from 'react-router-dom'
+function ResultPage({queryString, setQueryString, searchResult, setSearchResult}) {
 
-    const [res, setRes] = useState(["res1", "res2", "res3", "res4", "res5", "res6", "res7", "res8", "res9", "res10"])
-
+    const hits = searchResult.hits.hits
     return (
         <div className="App">
             <div className="grid-container">
                 <div className="resultheader">
                     <Logo/>
                 </div>
-                <form className="commentForm"onSubmit={(e) => setQueryString(e.target.value)}>
-                    <input className="input"
-                        type='text'
-                        value={queryString}
-                        onChange={(e) => setQueryString(e.target.value)}
-                    />
-                </form>
+                <div className="resultSearchField">
+                    <SearchField queryString={queryString} setQueryString={setQueryString} setSearchResult={setSearchResult}/>
+                </div>
             </div>
-            <div className="ResultBox">
-                {res.map((r) => <div>{r}</div>)}
+            <div className="hits"> 
+                {hits.length === 0 ? <div>No results</div> : hits.map(h => <div>{h._source.podcast_name} : {h._source.data} <br/></div>)}
             </div>
         </div>
     );
